16427c08b75493f18f4b09f06b3b5459ba138018
Yannick Meine <yannick.meine@gmail.com>
2026-01-19 16:42:19 +0100

    WIP Add note regarding xmake's target selection

--- get-exe-path.lua
+++ get-exe-path.lua
@@ -4,6 +4,7 @@ import('core.project.project')
 import('core.project.config')
 
 
+
 ----------------------------------------------------------------------------------------------------
 -- 
 ----------------------------------------------------------------------------------------------------
@@ -27,6 +28,9 @@ function main()
 
     -- local target = project.target('es')
 
+    -- XXX 2026-01-19T04:02:15+01:00@Europe/Paris
+    -- Real logic from xmake — not exposed — to pick targets to run automatically can be seen in: 
+    -- https://github.com/xmake-io/xmake/blob/766a7a86a7af89c58038500985c9d8ec441de25f/xmake/actions/run/main.lua#L229-L237
     local targets = to_list(project.targets())
     local target = targets[1]
 
